5d913ce3d7e2668cf03d6fc1c34f326230117006
Yohan Tancrez <yohan.tancrez@epita.fr>
2020-06-11 02:18:09 +0200

    Removed unuseful files, quick react-router-dom setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './views/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <div className="p-10">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="pt-10 p-5 flex justify-between items-center">
+            <span className="font-bold">
+                TANCREZ Yohan
+            </span>
+            <span>
+                About me
+            </span>
+        </header>
+    )
+}
+
+export default Header;
