eceac16eaef92d62e474f50004b39bbddc537754
Yohan Tancrez <yohan.tancrez@epita.fr>
2020-06-25 20:51:34 +0200

    Save / Load the choosen theme, fixed a bug with the dark mode slider

--- public/Darkmode.js
+++ public/Darkmode.js
@@ -1,5 +1,14 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
