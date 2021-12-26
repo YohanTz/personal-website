b49411bbf3bb3bfe641fb7f768eeacc503198d35
Yohan_Tz <yohan.tancrez@epita.fr>
2021-12-26 04:20:15 +0100

    fix: warnings

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,11 @@
 import React from "react";
-import Layout from "./src/components/layout";
+// import Layout from "./src/components/layout";
 
 // Wraps every page in a component
 // export const wrapPageElement = ({ element }) => {
 //   return <Layout>{element}</Layout>;
 // };
+
+export const wrapPageElement = ({ element }) => {
+  return <React.Fragment>{element}</React.Fragment>;
+};
